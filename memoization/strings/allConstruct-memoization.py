b835f55ea2e07f1dd44064584ae9683bae04c746
demi2k-sudo <85375873+demi2k-sudo@users.noreply.github.com>
2024-04-09 22:25:26 +0530

    allConstruct-memoization

--- memoization/strings/allConstruct-memoization.py
+++ memoization/strings/allConstruct-memoization.py
@@ -1,5 +1,10 @@
-def allConstruct(target, wordBank):
-    
+def allConstruct(target, wordBank, memo=None):
+    if memo is None:
+        memo = {}
+        
+    if target in list(memo.keys()):
+        return memo[target]
+        
     if target == "":
         return [[]]
     
@@ -7,15 +12,17 @@ def allConstruct(target, wordBank):
     
     for i in wordBank:
         if target[:len(i)] == i or target == i:
-            res = allConstruct(target[len(i):],wordBank)
+            res = allConstruct(target[len(i):],wordBank,memo)
             for j in res:
                 j.insert(0,i)
             final+=res
+    memo[target] = final
     return final
 
 print(allConstruct("abcdef",["ab","abc","cd","def","ef","abcd"]))
 print(allConstruct("purple",["purp","p","ur","le","purpl"]))
 print(allConstruct("skateboard",["bo","rd","ate","t","ska","sk","ate"]))
 
-# not possible
+# possible
 print(allConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",["ee","eee","eeeeee","e","eeeee","eeeeeeee"]))
+print(allConstruct("eeeeeeeeef",["eee","eeeeee","f"]))
